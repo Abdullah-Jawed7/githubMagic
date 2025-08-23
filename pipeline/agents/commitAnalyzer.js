e79952e1a4572cb8fdd90ccbee104ddeda36f69f
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-08-23 16:45:05 +0500

    Analyzing commit

---
+++ pipeline/agents/commitAnalyzer.js
@@ -0,0 +1,41 @@
+// agents/commitAnalyzer.js
+import { Agent } from "@openai/agents";
+import { runner } from "../../config.js";
+import { z } from "zod";
+
+
+export async function analyzeCommit(commitMessage, commitDiff) {
+  const instructions = `
+You are Commit Analyzer.
+Look at this commit message and diff. Decide what update type it represents:
+- project update
+- skill update
+- education update
+- bio update
+- other
+
+Return JSON with fields: {type, title, description, link (if any), skills (if any)}.
+`
+const prompt = `
+Commit message:
+${commitMessage}
+
+Diff:
+${commitDiff}
+  `;
+
+  const AnalyzerResultType = z.object({
+  type: z.string(),
+  description: z.string(),
+  link:z.array(z.string()).optional(),
+  skills: z.array(z.string()).optional(),
+});
+
+const commitAnalyzer = new Agent({
+    name:"Commit Analyzer",
+    instructions:instructions,
+    outputType:AnalyzerResultType,
+})
+const result  = await runner.run(commitAnalyzer ,prompt )
+return result.finalOutput;
+}
